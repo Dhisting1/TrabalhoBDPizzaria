f913b369fbeff74b4491db59728fc304a4593c54
Lucas Fernando Gonlçaves Lima <lcs.lima0708@gmail.com>
2024-01-24 20:26:45 -0300

    att

--- trabalhobd_pizzaria.sql
+++ trabalhobd_pizzaria.sql
@@ -123,3 +123,85 @@ CREATE TABLE `trabalhobd_pizzaria`.`estoque` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
+
+INSERT INTO cliente (nome_cliente, telefone, endereco) VALUES ('João', '61 99111-1111', 'Rua 1');
+INSERT INTO cliente (nome_cliente, telefone, endereco) VALUES ('Maria', '61 99111-1112', 'Rua 2');
+INSERT INTO cliente (nome_cliente, telefone, endereco) VALUES ('José', '61 99111-1113', 'Rua 3');
+INSERT INTO cliente (nome_cliente, telefone, endereco) VALUES ('Ana', '61 99111-1114', 'Rua 4');
+INSERT INTO cliente (nome_cliente, telefone, endereco) VALUES ('Pedro', '61 99111-1115', 'Rua 5');
+
+INSERT INTO funcionarios (nome_funcionarios, cargo_funcionarios, data_contratacao) VALUES ('Maria', 'Atendente', '2020-03-02');
+INSERT INTO funcionarios (nome_funcionarios, cargo_funcionarios, data_contratacao) VALUES ('Rodrigo', 'Entregador', '2020-03-02');
+INSERT INTO funcionarios (nome_funcionarios, cargo_funcionarios, data_contratacao) VALUES ('Guilherme', 'Gerente', '2020-03-02');
+INSERT INTO funcionarios (nome_funcionarios, cargo_funcionarios, data_contratacao) VALUES ('João', 'Entregador', '2020-03-02');
+INSERT INTO funcionarios (nome_funcionarios, cargo_funcionarios, data_contratacao) VALUES ('Ana', 'Atendente', '2020-03-02');
+
+INSERT INTO pizza (nome_pizza, ingredientes_pizza, descricao, preco) VALUES ('Calabresa', 'Calabresa, Queijo', 'Pizza de Calabresa', 30.00);
+INSERT INTO pizza (nome_pizza, ingredientes_pizza, descricao, preco) VALUES ('Frango', 'Frango, Queijo', 'Pizza de Frango', 35.00);
+INSERT INTO pizza (nome_pizza, ingredientes_pizza, descricao, preco) VALUES ('Portuguesa', 'Presunto, Queijo, Ovo, Cebola, Tomate', 'Pizza Portuguesa', 40.00);
+INSERT INTO pizza (nome_pizza, ingredientes_pizza, descricao, preco) VALUES ('Mussarela', 'Mussarela, Queijo', 'Pizza de Mussarela', 30.00);
+INSERT INTO pizza (nome_pizza, ingredientes_pizza, descricao, preco) VALUES ('Marguerita', 'Mussarela, Tomate, Manjericão', 'Pizza Marguerita', 35.00);
+
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (10, 'Calabresa');
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (20, 'Queijo');
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (10, 'Frango');
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (10, 'Presunto');
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (10, 'Ovo'); 
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (10, 'Cebola');
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (10, 'Tomate');
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (10, 'Mussarela');
+INSERT INTO ingredientes (quantidade_ingredientes, nome_ingredientes) VALUES (10, 'Manjericão');
+
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (1, 1);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (1, 2);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (2, 3);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (2, 2);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (3, 4);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (3, 2);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (3, 5);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (3, 6);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (3, 7);
+INSERT INTO pizza_ingredientes (pizza_id_ingredientes, ingredientes_id_ingredientes) VALUES (3, 8);
+
+INSERT INTO pedidos (data_pedido, quantidade_pedidos, status_pedido, cliente_id_cliente, pizza_id_pizza, valor_pedido) VALUES ('2020-03-02', 1, 'Em andamento', 1, 1, 30.00);
+INSERT INTO pedidos (data_pedido, quantidade_pedidos, status_pedido, cliente_id_cliente, pizza_id_pizza, valor_pedido) VALUES ('2020-03-03', 1, 'Em andamento', 2, 2, 35.00);
+INSERT INTO pedidos (data_pedido, quantidade_pedidos, status_pedido, cliente_id_cliente, pizza_id_pizza, valor_pedido) VALUES ('2020-03-04', 1, 'Saiu para entrega', 3, 3, 40.00);
+INSERT INTO pedidos (data_pedido, quantidade_pedidos, status_pedido, cliente_id_cliente, pizza_id_pizza, valor_pedido) VALUES ('2020-03-05', 1, 'Entregue', 4, 4, 30.00);
+INSERT INTO pedidos (data_pedido, quantidade_pedidos, status_pedido, cliente_id_cliente, pizza_id_pizza, valor_pedido) VALUES ('2020-03-06', 1, 'Entregue', 5, 5, 35.00);
+
+INSERT INTO entregas (data_entrega, funcionarios_id_funcionarios, pedidos_id_pedidos) VALUES ('2020-03-02', 2, 1);
+INSERT INTO entregas (data_entrega, funcionarios_id_funcionarios, pedidos_id_pedidos) VALUES ('2020-03-03', 4, 2);
+INSERT INTO entregas (data_entrega, funcionarios_id_funcionarios, pedidos_id_pedidos) VALUES ('2020-03-04', 4, 3);
+INSERT INTO entregas (data_entrega, funcionarios_id_funcionarios, pedidos_id_pedidos) VALUES ('2020-03-05', 4, 4);
+INSERT INTO entregas (data_entrega, funcionarios_id_funcionarios, pedidos_id_pedidos) VALUES ('2020-03-06', 4, 5);
+
+INSERT INTO pagamentos (metodo_pagamento, valor_compra, data_pagamento, pedido_id_pedido, pizza_id_pizza) VALUES ('Dinheiro', 30.00, '2020-03-02', 1, 1);
+INSERT INTO pagamentos (metodo_pagamento, valor_compra, data_pagamento, pedido_id_pedido, pizza_id_pizza) VALUES ('Dinheiro', 35.00, '2020-03-03', 2, 2);
+INSERT INTO pagamentos (metodo_pagamento, valor_compra, data_pagamento, pedido_id_pedido, pizza_id_pizza) VALUES ('Dinheiro', 40.00, '2020-03-04', 3, 3);
+INSERT INTO pagamentos (metodo_pagamento, valor_compra, data_pagamento, pedido_id_pedido, pizza_id_pizza) VALUES ('Dinheiro', 30.00, '2020-03-05', 4, 4);
+INSERT INTO pagamentos (metodo_pagamento, valor_compra, data_pagamento, pedido_id_pedido, pizza_id_pizza) VALUES ('Dinheiro', 35.00, '2020-03-06', 5, 5);
+
+INSERT INTO fornecedor (nome_fornecedor, telefone, endereco) VALUES ('Fornecedor 1', '61 99111-2221', 'Rua 1');
+INSERT INTO fornecedor (nome_fornecedor, telefone, endereco) VALUES ('Fornecedor 2', '61 99221-1112', 'Rua 2');
+INSERT INTO fornecedor (nome_fornecedor, telefone, endereco) VALUES ('Fornecedor 3', '61 99331-3323', 'Rua 3');
+INSERT INTO fornecedor (nome_fornecedor, telefone, endereco) VALUES ('Fornecedor 4', '61 99441-4444', 'Rua 4');
+INSERT INTO fornecedor (nome_fornecedor, telefone, endereco) VALUES ('Fornecedor 5', '61 99551-5555', 'Rua 5');
+
+INSERT INTO estoque (quantidade_estoque, data_atualizacao, pizza_id_pizza, fornecedor_id_fornecedor) VALUES (10, '2020-03-02', 1, 1);
+INSERT INTO estoque (quantidade_estoque, data_atualizacao, pizza_id_pizza, fornecedor_id_fornecedor) VALUES (20, '2020-03-03', 2, 2);
+INSERT INTO estoque (quantidade_estoque, data_atualizacao, pizza_id_pizza, fornecedor_id_fornecedor) VALUES (10, '2020-03-04', 3, 3);
+INSERT INTO estoque (quantidade_estoque, data_atualizacao, pizza_id_pizza, fornecedor_id_fornecedor) VALUES (10, '2020-03-05', 4, 4);
+INSERT INTO estoque (quantidade_estoque, data_atualizacao, pizza_id_pizza, fornecedor_id_fornecedor) VALUES (10, '2020-03-06', 5, 5);
+
+
+
+SELECT * FROM cliente;
+SELECT * FROM funcionarios;
+SELECT * FROM pizza;
+SELECT * FROM ingredientes;
+SELECT * FROM pizza_ingredientes;
+SELECT * FROM pedidos;
+SELECT * FROM entregas;
+SELECT * FROM pagamentos;
+SELECT * FROM fornecedor;
+SELECT * FROM estoque;
